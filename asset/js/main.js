2091e19a76418b6fcf287ed8535e051701af2137
Fatih Kadir Akın <fatih.akin@markafoni.com>
2012-10-10 12:06:15 +0300

    send

---
+++ asset/js/main.js
@@ -0,0 +1,23 @@
+/**
+ * Özgür Web Günleri 2012 - Backbone ile Uygulama Geliştirme Uygulamalı Semineri
+ * 
+ * main.js - Ana uygulama modülü.
+ *
+ * Bu dosyada ana uygulama geliştirilecek. Backbone Twitter Client'a bağlanarak arama
+ * yapacak. Kullanıcıdan gelen hashtag ile o hashtag'ın geçtiği verileri listeleyecek.
+ *
+ *
+ * # Gereksinimler:
+ *
+ * ## API Doc
+ *
+ * GET http://search.twitter.com/search.json
+ *  ?q=<query>
+ *  &rpp=<tweets per page>
+ *  &include_entities=true
+ *  &callback=<jsonp callback>
+ *  &result_type=recent
+ *  
+ */
+
+// TODO: Kodlar atölye sırasında yazılacak.
